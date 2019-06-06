d04927b74608745412fcfbc7170596e509371919
ninglei1993 <928187057@qq.com>
2019-06-06 12:59:03 +0800

    mysql cases

---
+++ mysqlSyntax.sql
@@ -0,0 +1,28 @@
+use myblog;
+show tables;
+
+-- insert into users (username,`password`,realname) values ('zhangsan','123','张三');
+
+select * from blogs;
+-- select id,username from users;
+-- select * from users where username='zhangsan' or `password`='123';
+-- select * from users where username like '%s%';
+-- select * from users where password like '%1%' order by id desc;
+
+-- update users set realname='张三' where password='1234';
+-- update users set realname='李四2' where username ='lisi';
+
+-- SET SQL_SAFE_UPDATES=0;
+-- delete from users where username='zhangsan';
+
+-- select * from users where state<>'0';
+-- update users set state='1' where username='lisi';
+
+-- insert into blogs (title ,content,createtime,author) values('标题B','内容B',1557189313235,'lisi');
+
+-- select * from blogs order by createtime desc;
+
+-- delete from blogs where id='3';
+
+-- select * from users;
+-- select * from blogs;
